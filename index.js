64400f75e1c7ab48273867653efee4c9390770f8
zeffrybaronalafanta <zeffrybaron@gmail.com>
2022-11-09 18:22:19 +0700

    Add Featur Swagger

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ require('express-group-routes')
 
 const { notFound,error } = require('./src/middlewares/errorHandling.middleware')
 const routes = require('./src/routes/index.routes')
+const swaggerJson = require('./openapi.json')
+const swaggerUi = require('swagger-ui-express')
 const express = require('express')
 
 const app = express()
 const port = process.env.PORT || 4000
 
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerJson))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(routes)
